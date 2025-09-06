3f86583edcda05f9e3c0f5c2786a3d9b8a9b5e99
Divya-Sebastian10 <divya10sebastian@gmail.com>
2025-09-06 10:33:03 +0530

    Added viewset to profiles api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,1 +1,6 @@
-from django.r
+from rest_framework import serializers
+
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing out APIView"""
+    name = serializers.CharField(max_length=10)
